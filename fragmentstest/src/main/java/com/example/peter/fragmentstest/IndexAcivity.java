fa3b187d9ce9d92d180509c6b60f938e6365eb2c
Peter x230 windows <jilianggqq@163.com>
2015-10-08 16:35:36 -0700

    basic framework

--- fragmentstest/src/main/java/com/example/peter/fragmentstest/IndexAcivity.java
+++ fragmentstest/src/main/java/com/example/peter/fragmentstest/IndexAcivity.java
@@ -13,8 +13,8 @@ public class IndexAcivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index_acivity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
