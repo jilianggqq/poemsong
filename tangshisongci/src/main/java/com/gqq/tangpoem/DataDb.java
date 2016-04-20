397d5b83ee46d3c1d7e4d858b4c440cc84b6b45e
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2016-04-20 16:49:15 +0800

    custom android studio

--- tangshisongci/src/main/java/com/gqq/tangpoem/DataDb.java
+++ tangshisongci/src/main/java/com/gqq/tangpoem/DataDb.java
@@ -1,13 +1,12 @@
 package com.gqq.tangpoem;
 
-import java.lang.annotation.Retention;
-import java.security.PublicKey;
-import java.util.*;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
-import android.content.*;
-import android.database.*;
-import android.database.sqlite.*;
-import android.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataDb {
 	public static final String DATADB_NAME = "tangshi.db";
